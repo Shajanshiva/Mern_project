cd0fc107968862f8a5945c20812cdac6e88f80e2
Shajanshiva <shajanshiva28@gmail.com>
2026-03-16 14:48:26 +0530

    create routes folder to change the path and element from app.jsx to routes folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import LoginPage from './pages/LoginPage';
-import Index from './pages/Index';
-import SignupPage from './pages/SignupPage';
-import Shows from './pages/Shows';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Theaters from './pages/Theaters';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoginPage />}></Route>
-        <Route path='/index' element={<Index />}></Route>
-        <Route path='/signup' element={<SignupPage />}></Route>
-        <Route path='/shows' element={<Shows />}></Route>
-        <Route path = '/theaters/:movieId' element={<Theaters/>}></Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
-
   );
 };
 
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import LoginPage from '../pages/LoginPage';
+import Index from '../pages/Index';
+import SignupPage from '../pages/SignupPage';
+import Shows from '../pages/Shows';
+import { Routes, Route } from 'react-router-dom';
+import Theaters from '../pages/Theaters';
+
+const AppRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/index" element={<Index />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/shows" element={<Shows />}></Route>
+        <Route path="/theaters/:movieId" element={<Theaters />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default AppRoutes
